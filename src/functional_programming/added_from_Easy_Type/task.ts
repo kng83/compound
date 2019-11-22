1f0391b6ca9733acc48de331b0d053f7584f4bbf
kng83 <pawel.kng@gmail.com>
2019-11-22 02:58:03 +0100

    end

--- src/functional_programming/added_from_Easy_Type/task.ts
+++ src/functional_programming/added_from_Easy_Type/task.ts
@@ -3,16 +3,20 @@ import util from 'util';
 import fs from 'fs';
 import path from 'path';
 
+const reject = (err:any) => err;
+const result = (value:any)=>value;
+
 class Task<T> {
   public fork: <N, P>(reject: N, resolve: P) => Task<T>;
 
   constructor(fork:(reject: any, resolve: any) => Task<T>) {
+    console.log(fork.toString(),'^^^^^FORK$$$$$$$$$$$')
     this.fork = fork;
   }
 
-  [util.inspect.custom]() {
-    return 'Task(?)';
-  }
+  // [util.inspect.custom]() {
+  //   return 'Task(?)';
+  // }
 
   static rejected<N>(x: N) {
     return new Task((reject: (x: N) => Task<N>, _?: any) => reject(x));
@@ -25,8 +29,11 @@ class Task<T> {
 
   // ----- Functor (Task a)
   map<R>(fn:(value:T)=>R) {
-    console.log('this fork to string',this.fork.toString());
-    return new Task((reject, resolve) => this.fork(reject, compose(resolve,fn)));
+    return new Task((reject, resolve) =>{
+      console.log('this fork to string',this.fork.toString(),'-----',fn);
+        console.log(resolve,'after======',fn.toString())
+       return this.fork(reject, ()=>fn(resolve))}
+       );
   }
 
   // ----- Applicative (Task a)
@@ -39,26 +46,30 @@ class Task<T> {
     return new Task((reject, resolve) => this.fork(reject, (x:any) => fn(x).fork(reject, resolve)));
   }
 
-  // join() {
-  //   return this.chain(identity);
-  // }
+  join() {
+    return this.chain(identity);
+  }
+  tap(){
+    console.log('#############TAP',this,'############# TAP');
+    return this;
+  }
 }
 
 const readFile = (filename:string) => new Task((reject:any, result:any) => {
- let pathToReadme = path.join(__dirname,'../../../',filename);
-  console.log(reject.toString(),result.toString());
+  let pathToReadme = path.join(__dirname,'../../../',filename);
+ // console.log(reject.toString());
   fs.readFile(pathToReadme, (err, data) => {
-    console.log('this is incoming');
+  //  console.log('this is incoming');
     if(err){
-      console.log('this is err',err);
+   //   console.log('this is err',err);
       return reject(err);
     }else {
-    //  console.log('this is data',data.toString('ascii'));
-      return result(data.toString('ascii'));
+    // console.log('this is data',data.toString('ascii'),result,'----');
+      
+       return result(data.toString('ascii'));
     }
   }) as any as any;
 });
 
   //to nie wiem dlaczego nie dziala
- readFile('./readme.md').map(split('\n')).map(head).fork((err:any,val:any)=>console.log(err,val));
-
+ readFile('./readme.md').fork((err:any,value:any)=>console.log(err,value,'###this is OUTPUT ####'));
