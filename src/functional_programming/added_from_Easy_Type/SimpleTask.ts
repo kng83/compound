95479f59e3c43ef2b879f6eedaf54df1537fb5a9
kng83 <pawel.kng@gmail.com>
2019-11-23 20:42:55 +0100

    end

--- src/functional_programming/added_from_Easy_Type/SimpleTask.ts
+++ src/functional_programming/added_from_Easy_Type/SimpleTask.ts
@@ -1,31 +1,30 @@
 import fs from 'fs';
 import path from 'path';
-import { compose ,pipe} from './pipe';
+import { split, toUpper } from './common';
 
-type Fork<T> = (resolve: (value?: T) => void, reject: (reason?: any) => void) => void;
+type Fork<T> = (resolve: (value: T) => void, reject: (reason: any) => void) => void;
 
 class SimpleTask<T> {
   constructor(private fork: Fork<T>) { }
 
   // ----- Functor (SimpleTask a)
-  map <R extends any>(fn:((value?: T) => R)) {
+  map<R extends any>(fn: ((value: T) => R)):SimpleTask<R> {
     return new SimpleTask<R>((resolve, reject) => {
-      this.fork(pipe(fn,resolve),reject)
+      this.fork(value => resolve(fn(value)), reject); //here is pipe operator
     });
   }
 
-  then(callback: (value?: T) => void) {
-    console.log('&&&&&',this.fork.toString(),'&&&&&')
+  then(callback: (value: T) => void) {
     this.fork(
-      (value?: T) => callback(value),
+      (value: T) => callback(value),
       (err: any) => err);
     return this;
   }
 
-  catch(callback:(err:any)=>void){
+  catch(callback: (err: any) => void) {
     this.fork(
-      (value?:T)=>value,
-      (err:any)=>callback(err)
+      (value?: T) => value,
+      (err: any) => callback(err)
     )
   }
 
@@ -37,7 +36,7 @@ class SimpleTask<T> {
   // ----- Monad (SimpleTask a)
   chain(fn: any) {
     return new SimpleTask((resolve: any, reject: any) =>
-      this.fork( (x: any) => fn(x).fork(reject, resolve),reject));
+      this.fork((x: any) => fn(x).fork(reject, resolve), reject));
   }
 
   join() {
@@ -45,26 +44,35 @@ class SimpleTask<T> {
   }
 }
 
-let someSimple = { bobo: 3, name: 'ssss' };
+
+//** Simple object check for promise */
+let someSimple = { bobo: 3, name: 'Bobo kot jest super' };
 
 const readSome = <T>(value: T) => new SimpleTask<T>((resolve, reject) => {
   resolve(value);
 })
 
-readSome(someSimple).map((value) => value? .name).then(value=>console.log(value)).catch(console.log);
+readSome(someSimple)
+  .map((value) => value.name)
+  .map(toUpper)
+  .then(value => console.log(value))
+  .then(value => console.log(value,'2'))
+  .map(value=>value + 'i koty sa piekne')
+  .then(value => console.log(value,'2'))
+  .catch(console.log);
 
 
 //** Here is may promise example read from file  */
-const readFile = (filename:string) => new SimpleTask<string>((resolve:any, reject:any) => {
-  let pathToReadme = path.join(__dirname,'../../../',filename);
+const readFile = (filename: string) => new SimpleTask<string>((resolve: any, reject: any) => {
+  let pathToReadme = path.join(__dirname, '../../../', filename);
   fs.readFile(pathToReadme, (err, data) => {
-    if(err){
+    if (err) {
       return reject(err);
-    }else {
-       return resolve(data.toString('ascii'));
+    } else {
+      return resolve(data.toString('ascii'));
     }
   })
 });
 
-//to dziala i dziala dobrze
- //readFile('./readme.md').then(console.log).catch(err=>console.log(err,'this is error'));
+//to dziala i ma sie dobrze
+readFile('./readme.md').map(split('\n')).then(console.log).catch(err => console.log(err, 'this is error'));
