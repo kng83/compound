d7a440ed9f0c1a5cdb50788867e823c78be3e7bf
kng83 <pawel.kng@gmail.com>
2019-11-24 20:44:41 +0100

    end

--- src/functional_programming/added_from_Easy_Type/SimpleTask.ts
+++ src/functional_programming/added_from_Easy_Type/SimpleTask.ts
@@ -8,23 +8,23 @@ class SimpleTask<T> {
   constructor(private fork: Fork<T>) { }
 
   // ----- Functor (SimpleTask a)
-  map<R extends any>(fn: ((value: T) => R)):SimpleTask<R> {
+  map<R extends any>(fn: ((value: T) => R)): SimpleTask<R> {
     return new SimpleTask<R>((resolve, reject) => {
       this.fork(value => resolve(fn(value)), reject); //here is pipe operator
     });
   }
 
-  then(callback: (value: T) => void) {
+  then(resolve: (value: T) => void) {
     this.fork(
-      (value: T) => callback(value),
+      (value: T) => resolve(value),
       (err: any) => err);
     return this;
   }
 
-  catch(callback: (err: any) => void) {
+  catch(reject: (err: any) => void) {
     this.fork(
       (value?: T) => value,
-      (err: any) => callback(err)
+      (err: any) => reject(err)
     )
   }
 
@@ -47,18 +47,18 @@ class SimpleTask<T> {
 
 //** Simple object check for promise */
 let someSimple = { bobo: 3, name: 'Bobo kot jest super' };
-
 const readSome = <T>(value: T) => new SimpleTask<T>((resolve, reject) => {
   resolve(value);
 })
 
+//** Reading block with promise based api */
 readSome(someSimple)
   .map((value) => value.name)
   .map(toUpper)
   .then(value => console.log(value))
-  .then(value => console.log(value,'2'))
-  .map(value=>value + 'i koty sa piekne')
-  .then(value => console.log(value,'2'))
+  .then(value => console.log(value, '2'))
+  .map(value => value + ' i to bardzo grzeczny kot')
+  .then(value => console.log(value, '2'))
   .catch(console.log);
 
 
@@ -74,5 +74,41 @@ const readFile = (filename: string) => new SimpleTask<string>((resolve: any, rej
   })
 });
 
-//to dziala i ma sie dobrze
+//this is good and work well
 readFile('./readme.md').map(split('\n')).then(console.log).catch(err => console.log(err, 'this is error'));
+
+// Some pseudo code to describe pattern;
+/*
+
+class SimpleTask<T> {
+  constructor(private fork: Fork<T>) { }
+
+  map(fn) {
+    return new SimpleTask((resolve, reject) => {
+      this.fork(value => resolve(fn(value)), reject);
+    });
+  }
+
+  then(resolve) {
+    this.fork(
+      value=> resolve(value),
+      err => err);
+    return this;
+  }
+  
+  // to by nie dzialalo bo filename jest nieznany
+  // ale w momencie callback gdy jest aktywny to wywolywana jest funkcja resolve lub reject z danymi
+  // to sie dzieje gdy callback z czytania pliku sie wyzwoli on uruchomi funkcje resolve z then i tak dostaniemy
+  // wynik
+  
+  function resRej(resolve,reject){
+    fs.readFile(filename, (err, data) => {
+      if (err) return reject(err);
+      else return resolve(data);
+    })
+  }
+  const readFile = filename => new SimpleTask(resRej);
+
+readFile('./readme.md').map(split('\n')).then(console.log).catch(err => console.log(err, 'this is error'));
+
+*/
