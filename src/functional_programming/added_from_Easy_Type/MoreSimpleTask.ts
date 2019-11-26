52b77c36470851172be3a72135231a8242f0990b
kng83 <pawel.kng@gmail.com>
2019-11-26 12:40:11 +0100

    end

--- src/functional_programming/added_from_Easy_Type/MoreSimpleTask.ts
+++ src/functional_programming/added_from_Easy_Type/MoreSimpleTask.ts
@@ -3,46 +3,25 @@ import path from 'path';
 import { split, toUpper } from './common';
 
 
-class MoreSimpleTask<T> {
-  constructor(private fork:  (resolve: (value: T) => void) => void) { } //function (resolve){resolve(someSimple)}
-  
-  then(r: (value: T) => void) {
-    console.log(this.fork.toString());               //function (resolve){resolve(someSimple)}
-    console.log(r.toString());                       //function (value) { return console.log(value, '2'); }
-    this.fork(val => {  // function(value=>)
-        console.log('VALUE',val,'VALUE');
-        r(val)
-    })
-    return this;
-  }
-  
-  map<R>(fn: ((value: T) => R)): MoreSimpleTask<R> {
-    return new MoreSimpleTask<R>(resolve => {
-      this.fork(value => resolve(fn(value))); //here is pipe operator
-    });
-  }
+class MoreSimpleTask<T>{
+  constructor(private fork:(resolve: (value: T) => void)=>void){} // save reference to function
 
+  then(resolve:(value:T)=>void){
+    this.fork( value=>resolve(value)) //call fork and pass function
+  }
 }
 
-//** Simple object check for promise */
-let someSimple = { bobo: 3, name: 'Bobo kot jest super' };
-const readSome =  new MoreSimpleTask<typeof someSimple>(resolve=> {
-  resolve(someSimple);
-})
+let someObj = {name:'Pawel',age:4};
 
-//** Reading block with promise based api */
-readSome.then(value => console.log(value, '2'))
+let someMoreSimpleTask = new MoreSimpleTask<typeof someObj>(resolve=>resolve(someObj));
+someMoreSimpleTask.then(value =>console.log(value));
 
 
-//** Here is may promise example read from file  */
-const readFile = (filename: string) => new MoreSimpleTask<Buffer>(resolve=> {
-  let pathToReadme = path.join(__dirname, '../../../', filename);
+//** Tak to idzie
+let fork = function(resolve:any){ resolve(someObj)} // to jest zapisane w klasie jako fork
+let p = function(value:any){ console.log(value,'nr 2')} // to idzie do funkcji then
+fork(p); // wywolanie w then
 
-  fs.readFile(pathToReadme, (err, data) => {
-      return resolve(data);
-  })
-});
 
-//this is good and work well
-readFile('./readme.md').map(data=>data.toString('ascii')).map(split('\n')).then(console.log);
+
 
