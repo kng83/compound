d252fea8641f2245f46a090828683ee216b4ef9a
kng83 <pawel.kng@gmail.com>
2019-12-01 17:50:24 +0100

    end

--- src/functional_programming/added_from_Easy_Type/lift.ts
+++ src/functional_programming/added_from_Easy_Type/lift.ts
@@ -2,8 +2,11 @@
 
 //**Making an applicative functor */
 
-class ApplicativeArray<T> extends Array<T>{
-
+class ApplicativeArray<T extends (...args: any) => any> extends Array<T>{
+    private constructor(...args: T[]) {
+        super(...args);
+    }
+ 
     static of<T>(...items: T[]): ApplicativeArray<T> {
         let ret = Object.create(ApplicativeArray.prototype);
         ret.push(...items);
@@ -16,21 +19,23 @@ class ApplicativeArray<T> extends Array<T>{
     tail() {
         return this[this.length - 1];
     }
-    // Apply elements to functional array
-    ap< G >(xs: G[]) {
-        return this.reduce((acc:G[], currentFn:T) => acc.concat(xs.map(currentFn)), []) as {(arg:G):T}[]
+    ap<R extends Parameters<T>, G>(xs: R[]) {
+        return this.reduce((acc: G[], currentFn: T) => acc.concat(xs.map(currentFn)), [])
     }
 
 };
 
 let appArray = ApplicativeArray.of<(a: number) => number>(a => 2 + a);
-let someArr = appArray.ap(['8',4,4]);
-console.log(someArr);f
+let someArr = appArray.ap([3, 4, 4]);
+console.log(someArr);
 
-let next = ApplicativeArray.of(1,2,3);
+let next = ApplicativeArray.of(1, 2, 3);
 ;
 
 // let someA = [1,2,3].concat([1]);
 // console.log(someA);
+
+
+
 
 
